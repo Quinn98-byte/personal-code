1612b6ab702aa6554d0ec76ba0f24bbc458fcac9
Quinn Sullivan <qs3406@rit.edu>
2023-03-10 11:36:31 -0500

    Migrating visual studio code.

---
+++ dictionary testing/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace dictionary_testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new dictionary that maps
+            // strings to other strings
+            Dictionary<String, String> dict = new Dictionary<String, String>();
+
+            // Add some data
+            dict.Add("Pax", "123-4567");
+            dict.Add("Pax?", "cell number");
+            dict.Add("Shiro", "555-5555");
+            dict["Jenny"] = "867-5309";
+
+            Console.WriteLine("Pax’s number: " + dict["Pax"]);
+
+            // Check to see if the key "Jenny" is in
+            // the dictionary before attempting to
+            // retrieve it
+            if (dict.ContainsKey("Jenny"))
+            {
+                Console.WriteLine(dict["Jenny"]);
+            }
+
+        }
+    }
+}
